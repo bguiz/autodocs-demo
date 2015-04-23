4ab6c371ef2e6cb8695b8ed924d68a867cfbb70e
Brendan Graetz <github@bguiz.com>
2015-04-23 17:02:44 +1000

    =BG= add date and time to the nearest hour to page so that republishing can occur more easily

--- generatedocs.js
+++ generatedocs.js
@@ -7,8 +7,18 @@ var packageJson = require('./package.json');
 var contents = [
   '<p>'+packageJson.name+'</p>',
   '<p>'+packageJson.description+'</p>',
-  '<p>'+packageJson.version+'</p>',
+  '<p>v'+packageJson.version+'</p>',
+  '<p>'+getDateRoundHour()+'</p>',
   ''
 ].join('\n');
 
 fs.writeFileSync('documentation/index.html', contents);
+
+function getDateRoundHour() {
+  var date = (new Date())
+    .toISOString()
+    .slice(0, 13)
+    .replace( 'T' , ':' )
+    ;
+  return date+':00';
+}
